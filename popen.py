d355f81c734799c917e7e21eaa08daad93ead341
Marco Nie <github@niekun.net>
2019-11-26 16:55:45 +0800

    test

--- popen.py
+++ popen.py
@@ -1,6 +1,6 @@
 from subprocess import Popen, PIPE
 
-site = 'niekun.net/manual/FANUC-INTERNAL'
+site = 'https://niekun.net/index.php/2019/10/'
 path = '/mnt/c/Users/HJ_NK/Documents/'
 
 print('downloading')
@@ -8,15 +8,16 @@ print('downloading')
 cmd = 'cd .. && cd {} && sudo wget -m -p -k {}'.format(path, site)
 #cmd = 'cd .. && pwd'
 p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
-#it = iter(bytes(p[0], 'utf-8').split('\n'))
+#it = iter(p[1].decode("utf-8").split('\n'))
 
 try:
     file1 = open("file.txt", "w")
-    file1.write("{}\n".format(str(p[1], 'utf-8')))
-    '''
-    for line in p[0].decode("utf-8").split('\n'):
+    #file1.write("{}\n".format(str(p[1], 'utf-8')))
+    
+    for line in p[1].decode("utf-8", 'ignore').split('\n'):
         file1.write("{}\n".format(line))
-        while "FINISHED" in line: print('finished')
-    '''
+        if "FINISHED" in line: 
+            print('finished')
+    
 finally:
     file1.close()
